26022716d88ec3340d07bd841795c79052f82ba6
Connor Ullmann <c.ullmann@criteo.com>
2018-07-24 23:31:45 -0400

    add hsv and golden ratio-based random color algorithm

--- color.js
+++ color.js
@@ -29,11 +29,20 @@ Color.getRandomColorSameLightnessFromString = function(string)
 
 Color.getRandomColorNormal = function(minRGBvalue=0, maxRGBvalue = 256)
 {
-	var diff = maxRGBvalue - minRGBvalue;
+	let diff = maxRGBvalue - minRGBvalue;
 	return new Color(Math.floor(Math.random() * diff + minRGBvalue),
 		Math.floor(Math.random() * diff + minRGBvalue),
 		Math.floor(Math.random() * diff + minRGBvalue));
 };
+
+var startValueRandomColorGoldenRatio = Math.random();
+Color.sampleRandomColorGoldenRatio = function(saturation, value)
+{//https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+    startValueRandomColorGoldenRatio += 0.61803398875;
+    startValueRandomColorGoldenRatio %= 1;
+    return Color.hsvToColor(startValueRandomColorGoldenRatio, saturation, value);
+};
+
 
 https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 Color.componentToHex = function(c)
@@ -59,4 +68,33 @@ Color.hexToColor = function(hex)
 
     let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? new Color(parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)) : null;
-}
+};
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR
+ * h, s, v
+*/
+Color.hsvToColor = function(h, s, v)
+{//https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+    let r, g, b, i, f, p, q, t;
+    if (arguments.length === 1)
+    {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6)
+	{
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    };
+    return new Color(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255));
+};
