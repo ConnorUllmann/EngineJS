c5aedd0db34583f985e97fe95198334a794d88b8
Connor Ullmann <c.ullmann@criteo.com>
2019-11-09 19:58:25 -0500

    allow collidesCircle to consider the origin to be the upper-left corner or the center of the rectangle when resolving collisions

--- rectangle.js
+++ rectangle.js
@@ -59,14 +59,14 @@ Rectangle.prototype.collidesRectangle = function(rectangle)
 //https://stackoverflow.com/questions/401847/circle-rectangle-collision-detection-intersection
 // The rectangle's (x, y) position is its top-left corner if it were not rotated,
 // however the rectangle still rotates about its center (by "rectangleAngleRadians" radians)
-Rectangle.prototype.collidesCircle = function(xCircle, yCircle, radius, rectangleAngleRadians=0)
+Rectangle.prototype.collidesCircle = function(xCircle, yCircle, radius, rectangleAngleRadians=0, rectangleIsCentered=false)
 {
     const circlePosition = rectangleAngleRadians === 0
         ? new Point(xCircle, yCircle)
-        : new Point(xCircle, yCircle).rotate(-rectangleAngleRadians, this.center);
+        : new Point(xCircle, yCircle).rotate(-rectangleAngleRadians, rectangleIsCentered ? this : this.center);
 
-    let xCircleDistance = Math.abs(circlePosition.x - this.xCenter);
-    let yCircleDistance = Math.abs(circlePosition.y - this.yCenter);
+    let xCircleDistance = Math.abs(circlePosition.x - (rectangleIsCentered ? this.x : this.x + this.w/2));
+    let yCircleDistance = Math.abs(circlePosition.y - (rectangleIsCentered ? this.y : this.y + this.h/2));
 
     if (xCircleDistance > (this.w/2 + radius)) { return false; }
     if (yCircleDistance > (this.h/2 + radius)) { return false; }
@@ -80,6 +80,46 @@ Rectangle.prototype.collidesCircle = function(xCircle, yCircle, radius, rectangl
 
     return cornerDistanceSq <= (radius * radius);
 };
+
+// World.prototype.debugDrawRectangleCollidesCircleTopLeft = function()
+// {
+//     const angle = this.world.millisecondsSinceStart / 5000 * Utils.TAU;
+//     const rectangle = new Rectangle(600, 450, 150, 100);
+//     const rectanglePosition = this.world.camera.add(rectangle);
+//     Draw.rectangle(this.world, rectanglePosition.x, rectanglePosition.y, rectangle.w, rectangle.h, new Color(255, 0, 0));
+//     Draw.rectangleRotated(this.world, rectanglePosition.x + rectangle.w/2, rectanglePosition.y + rectangle.h/2, rectangle.w, rectangle.h, new Color(0, 255, 0), angle);
+//
+//     const circlePosition = this.world.camera.add(this.world.mouse);
+//     const circleRadius = 50;
+//     const circleTranslatedPosition = circlePosition.rotate(-angle, rectanglePosition.add(new Point(rectangle.w/2, rectangle.h/2)));
+//
+//     Draw.circle(this.world, rectanglePosition.x, rectanglePosition.y, 6, new Color(255, 255, 0));
+//
+//     const circleLocal = circlePosition.subtract(this.world.camera);
+//     const collides = rectangle.collidesCircle(circleLocal.x, circleLocal.y, circleRadius, angle, false);
+//     Draw.circle(this.world, circlePosition.x, circlePosition.y, circleRadius, collides ? new Color(255, 255, 255) : new Color(0, 0, 255));
+//     Draw.circle(this.world, circleTranslatedPosition.x, circleTranslatedPosition.y, circleRadius, collides ? new Color(255, 200, 255) : new Color(255, 0, 255));
+// };
+//
+// World.prototype.debugDrawRectangleCollidesCircleCentered = function()
+// {
+//     const angle = this.world.millisecondsSinceStart / 5000 * Utils.TAU;
+//     const rectangle = new Rectangle(600, 450, 150, 100);
+//     const rectanglePosition = this.world.camera.add(rectangle);
+//     Draw.rectangle(this.world, rectanglePosition.x - rectangle.w/2, rectanglePosition.y - rectangle.h/2, rectangle.w, rectangle.h, new Color(255, 0, 0));
+//     Draw.rectangleRotated(this.world, rectanglePosition.x, rectanglePosition.y, rectangle.w, rectangle.h, new Color(0, 255, 0), angle);
+//
+//     const circlePosition = this.world.camera.add(this.world.mouse);
+//     const circleRadius = 50;
+//     const circleTranslatedPosition = circlePosition.rotate(-angle, rectanglePosition);
+//
+//     Draw.circle(this.world, rectanglePosition.x, rectanglePosition.y, 6, new Color(255, 255, 0));
+//
+//     const circleLocal = circlePosition.subtract(this.world.camera);
+//     const collides = rectangle.collidesCircle(circleLocal.x, circleLocal.y, circleRadius, angle, true);
+//     Draw.circle(this.world, circlePosition.x, circlePosition.y, circleRadius, collides ? new Color(255, 255, 255) : new Color(0, 0, 255));
+//     Draw.circle(this.world, circleTranslatedPosition.x, circleTranslatedPosition.y, circleRadius, collides ? new Color(255, 200, 255) : new Color(255, 0, 255));
+// };
 
 Rectangle.prototype.corners = function()
 {
